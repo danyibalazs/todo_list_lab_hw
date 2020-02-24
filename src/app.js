51517a95d747ee54134f7403750c896ebc396482
danyibalazs <danyi.balage@gmail.com>
2020-02-24 15:45:59 +0000

    finished hw

--- src/app.js
+++ src/app.js
@@ -4,10 +4,21 @@ document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: "#app",
     data: {
-
+      todos: [
+        {name: "Buy shopping", priority: "high" },
+        {name: "Clean bathroom", priority: "low" },
+        {name: "Car's MOT", priority: "high" }
+      ],
+      newItem: "",
+      priority: ""
     },
     methods: {
-
+      saveNewItem: function () {
+        this.todos.push({name: this.newItem,
+        priority: this.priority });
+        this.newItem = "";
+        this.priority= "";
+      }
     }
   });
 });
